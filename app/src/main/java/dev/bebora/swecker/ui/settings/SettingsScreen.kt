7b4041b8a4563fd1a97b11dbbbf811cb3adb257d
Ilogir <alfarigolix@gmail.com>
2022-08-22 18:42:12 +0000

    Fix setting screen blinking

--- app/src/main/java/dev/bebora/swecker/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/bebora/swecker/ui/settings/SettingsScreen.kt
@@ -2,12 +2,8 @@ package dev.bebora.swecker.ui.settings
 
 import android.app.Application
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -15,16 +11,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.bebora.swecker.data.service.impl.AccountsServiceImpl
 import dev.bebora.swecker.data.service.impl.AuthServiceImpl
 import dev.bebora.swecker.data.service.impl.ImageStorageServiceImpl
-import dev.bebora.swecker.data.service.impl.AccountsServiceImpl
 import dev.bebora.swecker.data.settings.DataStoreManager
 import dev.bebora.swecker.data.settings.Settings
 import dev.bebora.swecker.ui.settings.account.AccountDummyScreen
 import dev.bebora.swecker.ui.settings.main.SettingsDummyScreen
 import dev.bebora.swecker.ui.settings.sounds.SoundsDummyScreen
 import dev.bebora.swecker.ui.settings.theme.ThemeDummyScreen
-import dev.bebora.swecker.ui.theme.SettingsAwareTheme
 import dev.bebora.swecker.ui.theme.SweckerTheme
 
 @Composable
@@ -35,95 +30,96 @@ fun SettingsScreen(
 ) {
     val settingsState by viewModel.settings.collectAsState(initial = Settings())
     //TODO find a way to prevent wrong UI settings from appearing (look at collectAsState with wrong initial value)
-    SettingsAwareTheme(
-        darkModeType = settingsState.darkModeType,
-        palette = settingsState.palette
-    ) {
-        BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
-            if (maxWidth < 840.dp) {
-                if (viewModel.uiState.openAccountSettings) {
-                    BackHandler {
-                        viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                    }
-                    AccountDummyScreen(
-                        ui = viewModel.uiState,
-                        onEvent = viewModel::onEvent,
-                        uiEvent = viewModel.accountUiEvent,
-                        onNavigate = { onNavigate(it) }
-                    )
-                } else if (viewModel.uiState.openSoundsSettings) {
-                    BackHandler {
-                        viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                    }
-                    SoundsDummyScreen(
-                        settings = settingsState,
-                        ui = viewModel.uiState,
-                        onEvent = viewModel::onEvent
-                    )
-                } else if (viewModel.uiState.openThemeSettings) {
-                    BackHandler {
-                        viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                    }
-                    ThemeDummyScreen(
-                        settings = settingsState,
-                        ui = viewModel.uiState,
-                        onEvent = viewModel::onEvent
-                    )
-                } else {
-                    SettingsDummyScreen(
-                        settings = settingsState,
-                        ui = viewModel.uiState,
-                        onEvent = viewModel::onEvent,
-                        onGoBack = onGoBack,
-                        onNavigate = onNavigate
-                    )
+
+    BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+        if (maxWidth < 840.dp) {
+            if (viewModel.uiState.openAccountSettings) {
+                BackHandler {
+                    viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
                 }
+                AccountDummyScreen(
+                    ui = viewModel.uiState,
+                    onEvent = viewModel::onEvent,
+                    uiEvent = viewModel.accountUiEvent,
+                    onNavigate = { onNavigate(it) }
+                )
+            } else if (viewModel.uiState.openSoundsSettings) {
+                BackHandler {
+                    viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
+                }
+                SoundsDummyScreen(
+                    settings = settingsState,
+                    ui = viewModel.uiState,
+                    onEvent = viewModel::onEvent
+                )
+            } else if (viewModel.uiState.openThemeSettings) {
+                BackHandler {
+                    viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
+                }
+                ThemeDummyScreen(
+                    settings = settingsState,
+                    ui = viewModel.uiState,
+                    onEvent = viewModel::onEvent
+                )
             } else {
-                Row {
-                    SettingsDummyScreen(
-                        settings = settingsState,
-                        ui = viewModel.uiState,
-                        onEvent = viewModel::onEvent,
-                        modifier = Modifier.weight(1f),
-                        onGoBack = onGoBack,
-                        onNavigate = onNavigate
-                    )
-                    Box(modifier = Modifier.weight(1f)) {
-                        if (viewModel.uiState.openAccountSettings) {
-                            BackHandler {
-                                viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                            }
-                            AccountDummyScreen(
-                                ui = viewModel.uiState,
-                                onEvent = viewModel::onEvent,
-                                uiEvent = viewModel.accountUiEvent,
-                                onNavigate = { onNavigate(it) }
-                            )
-                        } else if (viewModel.uiState.openSoundsSettings) {
-                            BackHandler {
-                                viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                            }
-                            SoundsDummyScreen(
-                                settings = settingsState,
-                                ui = viewModel.uiState,
-                                onEvent = viewModel::onEvent
-                            )
-                        } else if (viewModel.uiState.openThemeSettings) {
-                            BackHandler {
-                                viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
-                            }
-                            ThemeDummyScreen(
-                                settings = settingsState,
-                                ui = viewModel.uiState,
-                                onEvent = viewModel::onEvent
-                            )
+                SettingsDummyScreen(
+                    settings = settingsState,
+                    ui = viewModel.uiState,
+                    onEvent = viewModel::onEvent,
+                    onGoBack = onGoBack,
+                    onNavigate = onNavigate
+                )
+            }
+        } else {
+            Row(
+                modifier = Modifier
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                SettingsDummyScreen(
+                    settings = settingsState,
+                    ui = viewModel.uiState,
+                    onEvent = viewModel::onEvent,
+                    modifier = Modifier.weight(1f),
+                    onGoBack = onGoBack,
+                    onNavigate = onNavigate
+                )
+                Box(modifier = Modifier.weight(1f)) {
+                    if (viewModel.uiState.openAccountSettings) {
+                        BackHandler {
+                            viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
                         }
+                        AccountDummyScreen(
+                            ui = viewModel.uiState,
+                            onEvent = viewModel::onEvent,
+                            uiEvent = viewModel.accountUiEvent,
+                            onNavigate = { onNavigate(it) }
+                        )
+                    } else if (viewModel.uiState.openSoundsSettings) {
+                        BackHandler {
+                            viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
+                        }
+                        SoundsDummyScreen(
+                            settings = settingsState,
+                            ui = viewModel.uiState,
+                            onEvent = viewModel::onEvent
+                        )
+                    } else if (viewModel.uiState.openThemeSettings) {
+                        BackHandler {
+                            viewModel.onEvent(SettingsEvent.CloseSettingsSubsection)
+                        }
+                        ThemeDummyScreen(
+                            settings = settingsState,
+                            ui = viewModel.uiState,
+                            onEvent = viewModel::onEvent,
+                        )
                     }
                 }
             }
         }
     }
 }
+
 
 @Preview
 @Composable
