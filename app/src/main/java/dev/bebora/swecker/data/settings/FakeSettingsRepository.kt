6996eeb824956b5a3ed03b3c4e850d0bf3fcc867
bebora <simone.rizzo@live.com>
2022-08-25 00:02:33 +0200

    Implement FakeSettingsRepository

--- app/src/main/java/dev/bebora/swecker/data/settings/FakeSettingsRepository.kt
+++ app/src/main/java/dev/bebora/swecker/data/settings/FakeSettingsRepository.kt
@@ -4,26 +4,52 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class FakeSettingsRepository : SettingsRepositoryInterface {
+    private var fakeSettings = fakeSettingsDefault
+
     override suspend fun setPalette(palette: Palette) {
+        fakeSettings = fakeSettings.copy(
+            palette = palette
+        )
     }
 
     override suspend fun setDarkModeType(darkModeType: DarkModeType) {
+        fakeSettings = fakeSettings.copy(
+            darkModeType = darkModeType
+        )
     }
 
     override suspend fun setRingtone(ringtone: Ringtone) {
+        fakeSettings = fakeSettings.copy(
+            ringtone = ringtone
+        )
     }
 
     override suspend fun setRingtoneVolume(ringtoneVolume: Int) {
+        fakeSettings = fakeSettings.copy(
+            ringtoneVolume = ringtoneVolume
+        )
     }
 
     override suspend fun setRingtoneDuration(ringtoneDuration: RingtoneDuration) {
+        fakeSettings = fakeSettings.copy(
+            ringtoneDuration = ringtoneDuration
+        )
     }
 
     override suspend fun setVibration(enabled: Boolean) {
+        fakeSettings = fakeSettings.copy(
+            vibration = enabled
+        )
     }
 
     override fun getSettings(): Flow<Settings> {
-        val previewSettings = Settings(
+        return flow {
+            emit(fakeSettings)
+        }
+    }
+
+    companion object {
+        val fakeSettingsDefault = Settings(
             palette = Palette.VIOLET,
             darkModeType = DarkModeType.SYSTEM,
             ringtone = Ringtone.DEFAULT,
@@ -31,11 +57,5 @@ class FakeSettingsRepository : SettingsRepositoryInterface {
             ringtoneVolume = 50,
             vibration = true
         )
-
-        return flow {
-            while(true) {
-                emit(previewSettings)
-            }
-        }
     }
 }
